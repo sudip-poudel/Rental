4e770030dafd2b9020b87a319c047e01c2e75db8
Sudip <sudippoudel181@gmail.com>
2024-05-18 18:00:57 +0545

    frontend initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>Test</div>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/vite.config.ts
@@ -0,0 +1,13 @@
+import path from "path";
+import tailwindcss from "tailwindcss";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
