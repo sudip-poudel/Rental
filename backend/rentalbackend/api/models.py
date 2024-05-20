dc07f218dc51e0d57daacb8ada8d37325f5cd2af
Sudip <sudippoudel181@gmail.com>
2024-05-20 13:34:03 +0545

    backend initial setup

---
+++ backend/rentalbackend/api/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+class Testing(models.Model):
+    name=models.TextField()
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import "./App.css";
 import { Button } from "@/components/ui/button";
 
