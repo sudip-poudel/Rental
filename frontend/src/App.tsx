2e49fd019b596edbb635ef4ecfd3bfa7cfa47356
Sudip <sudippoudel181@gmail.com>
2024-05-25 15:35:40 +0545

    folder structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
-import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Button } from "@/components/ui/button";
+import Homepage from "./pages/Homepage";
+import ProtectedRoute from "./modules/ProtectedRoutes";
+import Signinpage from "./pages/Signinpage";
 
 function App() {
   return (
     <>
-      <div>Test</div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/signin" Component={Signinpage} />
+      </Routes>
     </>
   );
 }
---
+++ frontend/src/pages/Signinpage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Signinpage = () => {
+  return <div>Signinpage</div>;
+};
+
+export default Signinpage;
