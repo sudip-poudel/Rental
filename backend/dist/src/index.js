769bba4c53e484de338685c161c1f8b0b2782abc
Sudip <sudippoudel181@gmail.com>
2024-06-21 21:58:58 +0545

    ffff

--- backend/dist/src/index.js
+++ backend/dist/src/index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-    origin: "*",
+    origin: ["http://localhost:5173", "https://rental-ruby.vercel.app"],
     credentials: true,
     METHODS: "GET, POST, PUT, DELETE",
 }));
