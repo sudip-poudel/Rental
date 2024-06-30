b4a7992aae05510a55b3daeac14d6d6219b67206
sudip-poudel <89255340+sudip-poudel@users.noreply.github.com>
2024-06-30 16:29:27 +0545

    cookie option

--- backend/src/helper/loginHelper.ts
+++ backend/src/helper/loginHelper.ts
@@ -1,5 +1,6 @@
 import { Response } from "express";
 import { generateToken } from "./generateToken";
+import { BACKEND_URL } from "../config";
 
 export const loginHelper = (
   user: {
@@ -18,9 +19,17 @@ export const loginHelper = (
 
   res.cookie("token", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
+    domain: BACKEND_URL,
+    secure: true,
+    sameSite: "none",
+    path: "/",
   });
   res.cookie("userdata", stringifiedUserData, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
+    domain: BACKEND_URL,
+    secure: true,
+    sameSite: "none",
+    path: "/",
   });
 
   return { success: true };
