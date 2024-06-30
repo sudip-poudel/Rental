b2182f2124529c9b7dd443967a14ffc0d83fe79d
sudip-poudel <89255340+sudip-poudel@users.noreply.github.com>
2024-06-30 16:31:38 +0545

    forget to build again

--- backend/dist/src/helper/loginHelper.js
+++ backend/dist/src/helper/loginHelper.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.loginHelper = void 0;
 const generateToken_1 = require("./generateToken");
+const config_1 = require("../config");
 const loginHelper = (user, res) => {
     const token = (0, generateToken_1.generateToken)({ id: user.id });
     const stringifiedUserData = JSON.stringify({
@@ -11,9 +12,17 @@ const loginHelper = (user, res) => {
     });
     res.cookie("token", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
+        domain: config_1.BACKEND_URL,
+        secure: true,
+        sameSite: "none",
+        path: "/",
     });
     res.cookie("userdata", stringifiedUserData, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
+        domain: config_1.BACKEND_URL,
+        secure: true,
+        sameSite: "none",
+        path: "/",
     });
     return { success: true };
 };
