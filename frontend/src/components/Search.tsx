60107a5d56512b559aad8f350dfc8851660738a1
sudip-poudel <89255340+sudip-poudel@users.noreply.github.com>
2024-06-30 18:13:22 +0545

    schtrm

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -7,10 +7,7 @@ import { useNavigate } from "react-router-dom";
 const Search = () => {
   const [searchItem, setSearchItem] = useState("");
 
-
-
   const navigate = useNavigate();
-
 
   const handleSearch = (e) => {
     setSearchItem(e.target.value);
@@ -19,10 +16,7 @@ const Search = () => {
 
   const handleSearchSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    navigate('/searchresults?searchterm='+searchItem,{replace: true});
-    
-    console.log("Search submitted:", searchItem);
-    
+    navigate("/searchresults?searchterm=" + searchItem, { replace: true });
   };
 
   return (
@@ -38,14 +32,18 @@ const Search = () => {
           <span>the perfect item for you.</span>
         </p>
         <div className="flex flex-row w-full mt-16 gap-5">
-          <form onSubmit={handleSearchSubmit}>
+          <form
+            onSubmit={handleSearchSubmit}
+            className="w-full flex flex-row gap-1"
+          >
             <input
               type="text"
               className="w-3/4 h-12 border-gray-400 border-[0.1px]  rounded-2xl pl-2"
-              placeholder= "Search for items..."
+              placeholder="Search for items..."
               onChange={handleSearch}
+              required
             />
-            <Button type="submit" className="font-bold h-12 w-1/4">
+            <Button type="submit" className="font-bold h-12 w-1/4 ">
               Search
             </Button>
           </form>
